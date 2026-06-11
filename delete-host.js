9817dfc4720ac48c140859c35f90d9ebbe1b6049
ljhaesler <lithiumium@gmail.com>
2026-06-11 16:32:05 +0200

    update scripts to work with pocketbase instance

---
+++ delete-host.js
@@ -0,0 +1,38 @@
+import PocketBase from "pocketbase";
+import "dotenv/config";
+import { execSync } from "child_process";
+
+const repo = process.argv[2];
+const pb = new PocketBase("https://is-dev.applications.ws");
+
+await pb
+	.collection("_superusers")
+	.authWithPassword("ljhaesler@protonmail.com", process.env.PASSWORD);
+
+const hostData = await pb
+	.collection("hosts")
+	.getList(1, 1, { filter: `repo="${repo}"` });
+
+if (hostData.items.length === 1) {
+	const existingHost = hostData.items[0];
+
+	execSync(`sudo a2dissite ${existingHost.repo} ${existingHost.repo}-le-ssl`);
+	execSync(
+		`sudo rm /etc/apache2/sites-available/${existingHost.repo}.conf /etc/apache2/sites-available/${existingHost.repo}-le-ssl.conf`,
+	);
+	execSync(`docker stop ${existingHost.name}-container`);
+	execSync(`docker rm ${existingHost.name}-container`);
+	execSync(`docker rmi ${existingHost.name}-image`);
+	execSync(
+		`certbot delete --cert-name ${existingHost.name}.is-dev.applications.ws`,
+		{ stdio: ["pipe", "inherit", "inherit"], input: "y\n" },
+	);
+
+	await pb.collection("hosts").delete(existingHost.id);
+
+	execSync("apache2ctl restart");
+
+	process.exit(0);
+} else {
+	throw new Error(`Repo name ${repo} was not found!`);
+}

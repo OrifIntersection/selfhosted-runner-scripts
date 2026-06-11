b9614ae046cf35b7ceabfee0bde23022267f89d5
ljhaesler <lithiumium@gmail.com>
2026-06-11 16:32:38 +0200

    track files correctly

--- generate-host.js
+++ generate-host.js
@@ -1,30 +1,39 @@
-import hosts from '/root/scripts/hosts.json' with { type: 'json' }
-import { execSync } from 'child_process';
-import fs from 'fs';
+import PocketBase from "pocketbase";
+import "dotenv/config";
+import { execSync } from "child_process";
+import fs from "fs";
 
-// the script is intended to be called with a repo name 'node findhost.js REPO_NAME'
-//
-if (!hosts) throw new Error("Hosts file is empty!")
-const repo = process.argv.slice(2)[0];
-if (!repo) throw new Error("No repo specified!")
-const usedNames = hosts.usedNames;
-const usedRepos = hosts.usedRepos;
-const allHosts = hosts.hosts;
+const repo = process.argv[2];
+const pb = new PocketBase("http://127.0.0.1:8090");
+
+await pb
+	.collection("_superusers")
+	.authWithPassword("ljhaesler@protonmail.com", process.env.PASSWORD);
 
 // basically this code will check if a repo already exists, and if it does, return the subdomain name and the port for that repo. If it doesn't, it will generate a new subdomain name and port for that repo, and then create a new Apache directive for that subdomain.
-
-// a total of 1296 domains are possible with these imports
+// a total of 1296 domains are possible with these animal-adjective combinations
 // there shouldn't be any reason for them to run out
-const animals = hosts.animals;
-const adjectives = hosts.adjectives;
 
+const hosts = await pb.collection("hosts").getFullList();
+const hostData = await pb
+	.collection("hosts")
+	.getList(1, 1, { filter: `repo="${repo}"` });
+const animalsData = await pb.collection("animals").getFullList();
+const adjectivesData = await pb.collection("adjectives").getFullList();
+
+let existingHost = null;
+if (hostData.items.length === 1) existingHost = hostData.items[0];
+
+const animals = animalsData.map((el) => el.animal);
+const adjectives = adjectivesData.map((el) => el.adjective);
+const usedNames = hosts.map((el) => el.name);
 
 function generateName() {
-	if (usedNames.length >= animals.length * adjectives.length) throw new Error("There are no more unique name combinations.")
-
 	let exists = true;
 	let name;
 	while (exists) {
+		// this find algorithm gets exponentially worse once half the possibe names are used
+		// I would assume that there will never be more than 600 websites though...
 		const adjI = Math.floor(Math.random() * adjectives.length);
 		const animI = Math.floor(Math.random() * animals.length);
 
@@ -40,34 +49,33 @@ function generatePort() {
 		const usedPorts = getUsedPorts();
 
 		for (let port = 1024; port <= 65535; port++) {
-	    if (!usedPorts.includes(port)) {
-	      return port;
+			if (!usedPorts.includes(port)) {
+				return port;
 			}
 		}
 
-		throw new Error('No available ports found in the specified range');
+		throw new Error("No available ports found in the specified range");
 	} catch (err) {
 		throw new Error(err);
 	}
 }
 
-
 function getUsedPorts() {
- try {
-    const output = execSync('ss -tuln', { encoding: 'utf8' });
-    const ports = [];
+	try {
+		const output = execSync("ss -tuln", { encoding: "utf8" });
+		const ports = [];
 
-    output.split('\n').forEach(line => {
-      const match = line.match(/:(\d+)\s/);
-      if (match) {
-        ports.push(parseInt(match[1]));
-      }
-    });
+		output.split("\n").forEach((line) => {
+			const match = line.match(/:(\d+)\s/);
+			if (match) {
+				ports.push(parseInt(match[1]));
+			}
+		});
 
-    return ports;
-  } catch (err) {
-	 throw new Error(err);
-  }
+		return ports;
+	} catch (err) {
+		throw new Error(err);
+	}
 }
 
 function writeApacheDirective(name, port, repo) {
@@ -77,49 +85,47 @@ function writeApacheDirective(name, port, repo) {
 	    ProxyPreserveHost On
 	    ProxyPass / http://127.0.0.1:${port}/
 	    ProxyPassReverse / http://127.0.0.1:${port}/
-    </VirtualHost>`
+    </VirtualHost>`;
 
 	fs.writeFileSync(`/etc/apache2/sites-available/${repo}.conf`, directive);
 }
 
-function writeToJson(name, port, repo) {
-	hosts.hosts.push({ name, port, repo });
-	hosts.usedNames.push(name);
-	hosts.usedRepos.push(repo);
-
-	// writing this synchronously might cause latency issues if the JSON file gets very large (hundreds of hosts) but it's very unlikely that would happen, and this script would be run infrequently
-	fs.writeFileSync("/root/scripts/hosts.json", JSON.stringify(hosts, null, " "))
+async function writeToDb(name, port, repo) {
+	await pb.collection("hosts").create({
+		name,
+		port,
+		repo,
+	});
 }
 
 try {
-
 	let name;
 	let port;
+	let cert = "false";
 
-	if (usedRepos.includes(repo)) {
-		const existingHost = allHosts.find((host) => host.repo === repo);
+	if (existingHost) {
 		name = existingHost.name;
 		port = existingHost.port;
 	} else {
 		name = generateName();
 		port = generatePort();
-		writeToJson(name, port, repo)
-		writeApacheDirective(name, port, repo)
+		await writeToDb(name, port, repo);
+		writeApacheDirective(name, port, repo);
+		cert = "true";
 	}
 
 	if (process.env.GITHUB_OUTPUT) {
-	  const lines = [
-	    `domain-name=${name}`,
-	    `port=${port}`,
-	  ];
-	  fs.appendFileSync(process.env.GITHUB_OUTPUT, lines.join('\n') + '\n');
+		const lines = [`domain-name=${name}`, `port=${port}`, `cert=${cert}`];
+		fs.appendFileSync(process.env.GITHUB_OUTPUT, lines.join("\n") + "\n");
 	} else {
-		console.log(JSON.stringify({
-			name, port
-		}));
+		console.log(
+			JSON.stringify({
+				name,
+				port,
+				cert,
+			}),
+		);
 	}
-
-
 } catch (err) {
 	console.error(err);
 }
